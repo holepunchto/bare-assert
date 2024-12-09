3d883360469f3f3db765931e1c4516b5b5d4b84f
yasser <dev@yasser.email>
2024-12-09 18:13:25 -0300

    Add TypeScript typings (#1)

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+declare function assert(value: any, message?: string | Error): void
+
+declare namespace assert {
+  class AssertionError extends Error {
+    constructor(opts?: {
+      message?: string,
+      actual?: any,
+      expected?: any,
+      operator?: string
+    })
+
+    actual?: any
+    expected?: any
+    operator?: string
+  }
+}
+
+export = assert

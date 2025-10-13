e77dc27a9de20e4eecfdb6cbd45fdb1c6a918225
Yasser Nascimento <dev@yasser.email>
2025-10-13 12:15:30 -0300

    Switch to `prettier-config-holepunch`

--- index.d.ts
+++ index.d.ts
@@ -2,12 +2,7 @@ declare function assert(value: any, message?: string | Error): void
 
 declare namespace assert {
   export class AssertionError extends Error {
-    constructor(opts?: {
-      message?: string
-      actual?: any
-      expected?: any
-      operator?: string
-    })
+    constructor(opts?: { message?: string; actual?: any; expected?: any; operator?: string })
 
     actual?: any
     expected?: any
@@ -16,29 +11,13 @@ declare namespace assert {
 
   export function ok(value: any, message?: string | Error): void
 
-  export function equal(
-    actual: any,
-    expected: any,
-    message?: string | Error
-  ): void
+  export function equal(actual: any, expected: any, message?: string | Error): void
 
-  export function notEqual(
-    actual: any,
-    expected: any,
-    message?: string | Error
-  ): void
+  export function notEqual(actual: any, expected: any, message?: string | Error): void
 
-  export function strictEqual(
-    actual: any,
-    expected: any,
-    message?: string | Error
-  ): void
+  export function strictEqual(actual: any, expected: any, message?: string | Error): void
 
-  export function notStrictEqual(
-    actual: any,
-    expected: any,
-    message?: string | Error
-  ): void
+  export function notStrictEqual(actual: any, expected: any, message?: string | Error): void
 }
 
 export = assert

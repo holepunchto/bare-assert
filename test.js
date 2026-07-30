1fa781d170d96fc99ea592d7677e4efc9d47592c
Yasser Nascimento <dev@yasser.email>
2026-07-30 11:51:54 -0300

    Complete unit test coverage

--- test.js
+++ test.js
@@ -2,6 +2,42 @@ const test = require('brittle')
 const assert = require('.')
 
 test('basic', (t) => {
+  t.execution(() => assert(true))
   t.exception(() => assert(false, 'should fail'), /should fail/)
-  t.execution(() => assert(true, 'should pass'))
+})
+
+test('fail', (t) => {
+  t.exception(() => assert.fail('should fail'), /should fail/)
+})
+
+test('ok', (t) => {
+  t.execution(() => assert.ok(true))
+  t.exception(() => assert.ok(false, 'should fail'), /should fail/)
+})
+
+test('notOk', (t) => {
+  t.execution(() => assert.notOk(false))
+  t.exception(() => assert.notOk(true, 'should fail'), /should fail/)
+})
+
+test('equal', (t) => {
+  t.execution(() => assert.equal(1, '1'))
+  t.execution(() => assert.equal(NaN, NaN))
+  t.exception(() => assert.equal({}, {}, 'should fail'), /should fail/)
+})
+
+test('notEqual', (t) => {
+  t.execution(() => assert.notEqual({}, {}))
+  t.exception(() => assert.notEqual(1, '1', 'should fail'), /should fail/)
+  t.exception(() => assert.notEqual(NaN, NaN, 'should fail'), /should fail/)
+})
+
+test('strictEqual', (t) => {
+  t.execution(() => assert.strictEqual(1, 1))
+  t.exception(() => assert.strictEqual(1, '1', 'should fail'), /should fail/)
+})
+
+test('notStrictEqual', (t) => {
+  t.execution(() => assert.notStrictEqual(1, '1'))
+  t.exception(() => assert.notStrictEqual(1, 1, 'should fail'), /should fail/)
 })

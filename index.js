1637b70ca10f388ec33165273aacdc1bd89b7d57
Yasser Nascimento <dev@yasser.email>
2026-08-03 18:47:26 -0300

    Add `deepStrictEqual` support

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inspect = require('bare-inspect')
+const getType = require('bare-type')
 
 class AssertionError extends Error {
   constructor(opts = {}) {
@@ -106,4 +107,156 @@ exports.ifError = function ifError(actual) {
   const message = `ifError got ${inspect(actual)}`
 
   assertFail({ message, actual, operator: 'ifError' }, ifError)
+}
+
+exports.deepStrictEqual = function deepStrictEqual(actual, expected, message) {
+  if (deepStrictEqualValue(actual, expected)) return
+
+  assertFail({ message, actual, expected, operator: 'deepStrictEqual' }, deepStrictEqual)
+}
+
+exports.notDeepStrictEqual = function notDeepStrictEqual(actual, expected, message) {
+  if (!deepStrictEqualValue(actual, expected)) return
+
+  assertFail({ message, actual, expected, operator: 'notDeepStrictEqual' }, notDeepStrictEqual)
+}
+
+function deepStrictEqualValue(a, b) {
+  const type = getType(a)
+
+  if (!type.isObject() || !getType(b).isObject()) return Object.is(a, b)
+
+  if (Object.getPrototypeOf(a) !== Object.getPrototypeOf(b)) return false
+
+  const isWrapped = [
+    String.prototype,
+    Number.prototype,
+    Boolean.prototype,
+    Date.prototype
+  ].includes(Object.getPrototypeOf(a))
+
+  if (isWrapped) return deepStrictEqualValue(a.valueOf(), b.valueOf())
+
+  if (type.isWeakMap() || type.isWeakSet() || type.isPromise()) return a === b
+
+  if (type.isArray()) return deepStrictEqualArray(a, b)
+  if (type.isMap()) return deepStrictEqualMap(a, b)
+  if (type.isSet()) return deepStrictEqualSet(a, b)
+  if (type.isRegExp()) return deepStrictEqualRegexp(a, b)
+  if (type.isError()) return deepStrictEqualError(a, b)
+
+  return deepStrictEqualObject(a, b)
+}
+
+function deepStrictEqualArray(a, b) {
+  if (a.length !== b.length) return false
+
+  for (let i = 0; i < a.length; i++) {
+    if (!deepStrictEqualValue(a[i], b[i])) return false
+  }
+
+  return true
+}
+
+function deepStrictEqualMap(a, b) {
+  if (a.size !== b.size) return false
+
+  const nonPrimitiveKeysEntriesFromA = []
+
+  for (const [key, value] of a) {
+    if (getType(key).isObject()) nonPrimitiveKeysEntriesFromA.push([key, value])
+    else if (!b.has(key) || !deepStrictEqualValue(value, b.get(key))) return false
+  }
+
+  if (nonPrimitiveKeysEntriesFromA.length > 0) {
+    const nonPrimitiveKeysEntriesFromB = []
+
+    for (const [key, value] of b) {
+      if (getType(key).isObject()) nonPrimitiveKeysEntriesFromB.push([key, value])
+    }
+
+    if (nonPrimitiveKeysEntriesFromA.length !== nonPrimitiveKeysEntriesFromB.length) {
+      return false
+    }
+
+    for (const [keyA, valueA] of nonPrimitiveKeysEntriesFromA) {
+      let found = false
+
+      for (let i = 0; i < nonPrimitiveKeysEntriesFromB.length; i++) {
+        const [keyB, valueB] = nonPrimitiveKeysEntriesFromB[i]
+
+        if (deepStrictEqualValue(keyA, keyB) && deepStrictEqualValue(valueA, valueB)) {
+          nonPrimitiveKeysEntriesFromB.splice(i, 1)
+          found = true
+          break
+        }
+      }
+
+      if (found === false) return false
+    }
+  }
+
+  return true
+}
+
+function deepStrictEqualSet(a, b) {
+  if (a.size !== b.size) return false
+
+  const nonPrimitiveItemsFromA = []
+
+  for (const item of a) {
+    if (getType(item).isObject()) nonPrimitiveItemsFromA.push(item)
+    else if (!b.has(item)) return false
+  }
+
+  if (nonPrimitiveItemsFromA.length > 0) {
+    const nonPrimitiveItemsFromB = []
+
+    for (const item of b) {
+      if (getType(item).isObject()) nonPrimitiveItemsFromB.push(item)
+    }
+
+    if (nonPrimitiveItemsFromA.length !== nonPrimitiveItemsFromB.length) return false
+
+    for (const itemA of nonPrimitiveItemsFromA) {
+      let found = false
+
+      for (let i = 0; i < nonPrimitiveItemsFromB.length; i++) {
+        const itemB = nonPrimitiveItemsFromB[i]
+
+        if (deepStrictEqualValue(itemA, itemB)) {
+          nonPrimitiveItemsFromB.splice(i, 1)
+          found = true
+          break
+        }
+      }
+
+      if (found === false) return false
+    }
+
+    return nonPrimitiveItemsFromB.length === 0
+  }
+
+  return true
+}
+
+function deepStrictEqualRegexp(a, b) {
+  return a.lastIndex === b.lastIndex && a.flags === b.flags && a.source === b.source
+}
+
+function deepStrictEqualError(a, b) {
+  return deepStrictEqualValue(a.name, b.name) && deepStrictEqualValue(a.message, b.message)
+}
+
+function deepStrictEqualObject(a, b) {
+  const aKeys = [...Object.keys(a), ...Object.getOwnPropertySymbols(a)]
+  const bKeys = [...Object.keys(b), ...Object.getOwnPropertySymbols(b)]
+
+  if (aKeys.length !== bKeys.length) return false
+
+  for (const key of aKeys) {
+    if (!deepStrictEqualValue(a[key], b[key])) return false
+  }
+
+  return true
 }
--- test.js
+++ test.js
@@ -59,3 +59,156 @@ test('ifError', (t) => {
   t.execution(() => assert.ifError(undefined))
   t.exception(() => assert.ifError('error'))
 })
+
+test('deepStrictEqual, basic', (t) => {
+  t.execution(() => assert.deepStrictEqual(NaN, NaN))
+  t.execution(() => assert.deepStrictEqual(1, 1))
+  t.execution(() => assert.deepStrictEqual('foo', 'foo'))
+  t.exception(() => assert.deepStrictEqual(1, new Date(), 'should fail'), /should fail/)
+})
+
+test('deepStrictEqual, array', (t) => {
+  t.execution(() => assert.deepStrictEqual([1, 'foo'], [1, 'foo']))
+  t.exception(() => assert.deepStrictEqual([1, 'foo'], [1], 'should fail'), /should fail/)
+  t.exception(() => assert.deepStrictEqual([1, 'foo'], [1, 'bar'], 'should fail'), /should fail/)
+})
+
+test('deepStrictEqual, object', (t) => {
+  t.execution(() => assert.deepStrictEqual({}, {}))
+  t.execution(() => assert.deepStrictEqual({ a: { b: 1 } }, { a: { b: 1 } }))
+  t.execution(() => assert.deepStrictEqual({ a: [1, 2] }, { a: [1, 2] }))
+  t.exception(
+    () => assert.deepStrictEqual({ a: { b: 1 } }, { a: { b: '1' } }, 'should fail'),
+    /should fail/
+  )
+  t.exception(() => assert.deepStrictEqual({ a: [1, 2] }, { a: [1] }, 'should fail'), /should fail/)
+})
+
+test('deepStrictEqual, regexp', (t) => {
+  t.execution(() => assert.deepStrictEqual(/abc/, /abc/))
+  t.exception(() => assert.deepStrictEqual(/abc/, /abc/g, 'should fail'), /should fail/)
+})
+
+test('deepStrictEqual, map', (t) => {
+  t.execution(() =>
+    assert.deepStrictEqual(
+      new Map([
+        [{}, null],
+        [true, 2],
+        [undefined, {}]
+      ]),
+      new Map([
+        [undefined, {}],
+        [true, 2],
+        [{}, null]
+      ])
+    )
+  )
+  t.exception(
+    () =>
+      assert.deepStrictEqual(
+        new Map([
+          [{}, null],
+          [true, 2],
+          [undefined, {}]
+        ]),
+        new Map([
+          [{}, null],
+          [true, 2],
+          [null, {}] // different key
+        ]),
+        'should fail'
+      ),
+    /should fail/
+  )
+})
+
+test('deepStrictEqual, set', (t) => {
+  t.execution(() => assert.deepStrictEqual(new Set(['a', 1, 'b', 2]), new Set(['b', 2, 'a', 1])))
+  t.execution(() =>
+    assert.deepStrictEqual(new Set([{ a: 1 }, 1, {}, 2]), new Set([{}, 2, 1, { a: 1 }]))
+  )
+  t.exception(
+    () =>
+      assert.deepStrictEqual(new Set(['a', 1, 'b', 2]), new Set(['b', 2, 'a', 42]), 'should fail'),
+    /should fail/
+  )
+})
+
+test('deepStrictEqual, weak map', (t) => {
+  const map1 = new WeakMap([[Object, true]])
+  const map2 = new WeakMap([[Object, true]])
+
+  t.execution(() => assert.deepStrictEqual(map1, map1))
+  t.exception(() => assert.deepStrictEqual(map1, map2, 'should fail'), /should fail/)
+})
+
+test('deepStrictEqual, weak set', (t) => {
+  const obj = {}
+
+  const set1 = new WeakSet([obj])
+  const set2 = new WeakSet([obj])
+
+  t.execution(() => assert.deepStrictEqual(set1, set1))
+  t.exception(() => assert.deepStrictEqual(set1, set2, 'should fail'), /should fail/)
+})
+
+test('deepStrictEqual, symbols', (t) => {
+  t.execution(() => assert.deepStrictEqual(Symbol.for('foo'), Symbol.for('foo')))
+  t.exception(
+    () => assert.deepStrictEqual(Symbol.for('foo'), Symbol.for('bar'), 'should fail'),
+    /should fail/
+  )
+
+  const sym1 = Symbol()
+  const sym2 = Symbol()
+
+  t.execution(() => assert.deepStrictEqual({ [sym1]: 1 }, { [sym1]: 1 }))
+  t.exception(
+    () => assert.deepStrictEqual({ [sym1]: 1 }, { [sym2]: 1 }, 'should fail'),
+    /should fail/
+  )
+})
+
+test('deepStrictEqual, object wrappers', (t) => {
+  t.execution(() => assert.deepStrictEqual(new String('foo'), Object('foo')))
+  t.execution(() => assert.deepStrictEqual(new Number(1), new Number(1)))
+  t.exception(
+    () => assert.deepStrictEqual(new Number(1), new Number(2), 'should fail'),
+    /should fail/
+  )
+})
+
+test('deepStrictEqual, functions', (t) => {
+  t.execution(() => assert.deepStrictEqual(new Error('foo'), new Error('foo')))
+  t.exception(
+    () => assert.deepStrictEqual(new Error('foo'), new Error('bar'), 'should fail'),
+    /should fail/
+  )
+})
+
+test.skip('deepStrictEqual, recursive self-references', (t) => {
+  const foo = {}
+  foo.prop = foo
+
+  const bar = {}
+  bar.prop = bar
+
+  t.execution(() => assert.deepStrictEqual(foo, bar))
+})
+
+test.skip('deepStrictEqual, recursive mutual references', (t) => {
+  const foo = { prop: null }
+  const bar = { prop: foo }
+  foo.prop = bar
+
+  t.execution(() => assert.deepStrictEqual(foo, bar))
+})
+
+test.skip('deepStrictEqual, recursive lists', (t) => {
+  const foo = []
+  const bar = [foo]
+  foo[0] = bar
+
+  t.execution(() => assert.deepStrictEqual(foo, bar))
+})
